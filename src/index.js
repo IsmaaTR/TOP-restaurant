a215e19e33600d68cfc349b82cf80816b86c558e
Isma <IsmaaTR@users.noreply.github.com>
2025-03-22 14:31:54 +0100

    Modularized index.js. Added file structure to manage windows

--- src/index.js
+++ src/index.js
@@ -1,4 +1,37 @@
 import "./styles.css";
-import { UIModule } from "./UIModule.js"
+import { homeModule } from "./home.js";
 
-UIModule.init();
+const mainModule = (function() {
+    const contentDiv = document.querySelector('#content');
+    const homeButton = document.querySelector('#homeButton');
+    const menuButton = document.querySelector('#menuButton');
+    const contactButton = document.querySelector('#contactButton');
+
+    function init() {
+        setListeners();
+        //homeModule.init();
+    }
+
+    function cleanContent() {
+        contentDiv.replaceChildren(); 
+    }
+
+    function setListeners() {
+        homeButton.addEventListener('click', () => {
+            cleanContent();
+            homeModule.init();
+        });
+
+        menuButton.addEventListener('click', () => {
+            cleanContent();
+        });
+
+        contactButton.addEventListener('click', () => {
+            cleanContent();
+        });
+    }
+
+    return { init };
+})();
+
+mainModule.init();
